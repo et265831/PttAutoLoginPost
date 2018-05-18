9ef438136b34df19bb1fd534031fa40a6e66314d
Warren Cheng <warrenycheng@gmail.com>
2018-05-18 11:19:22 +0800

    logging

--- PttAuto.py
+++ PttAuto.py
@@ -1,7 +1,16 @@
+#!/usr/bin/python3
 import sys
 import telnetlib
 import time
+import logging
 
+
+logging.basicConfig(
+    filename="ptt_login.log",
+    level=logging.DEBUG,
+    format='%(asctime)s %(name)-5s %(levelname)-8s %(message)s',
+    datefmt='%y-%m-%d %H:%M'
+)
 
 class Ptt(object):
     def __init__(self, host, user, password):
@@ -14,24 +23,24 @@ class Ptt(object):
     @property
     def is_success(self):
         if u"密碼不對" in self._content:
-            print("密碼不對或無此帳號。程式結束")
+            logging.info("密碼不對或無此帳號。程式結束")
             sys.exit()
         if u"您想刪除其他重複登入" in self._content:
-            print("刪除其他重複登入的連線....")
+            logging.info("刪除其他重複登入的連線....")
             self._telnet.write(b"y\r\n")
             time.sleep(5)
             self._content = self._telnet.read_very_eager().decode('big5', 'ignore')
         if u"請按任意鍵繼續" in self._content:
-            print("in front page now...")
+            logging.info("in front page now...")
             self._telnet.write(b"\r\n")
             time.sleep(2)
         if u"您要刪除以上錯誤嘗試" in self._content:
-            print("刪除以上錯誤嘗試...")
+            logging.info("刪除以上錯誤嘗試...")
             self._telnet.write(b"y\r\n")
             time.sleep(2)
             self._content = self._telnet.read_very_eager().decode('big5', 'ignore')
         if u"您有一篇文章尚未完成" in self._content:
-            print('刪除尚未完成的文章....')
+            logging.info('刪除尚未完成的文章....')
             # 放棄尚未編輯完的文章
             self._telnet.write(b"q\r\n")
             time.sleep(2)
@@ -41,9 +50,9 @@ class Ptt(object):
     @property
     def input_user_password(self):
         if u"請輸入代號" in self._content:
-            print('input acct'.format(self._user))
+            logging.info('input acct'.format(self._user))
             self._telnet.write(self._user + b"\r\n")
-            print('input pw')
+            logging.info('input pw')
             self._telnet.write(self._password + b"\r\n")
             time.sleep(2)
             self._content = self._telnet.read_very_eager().decode('big5', 'ignore')
@@ -53,27 +62,27 @@ class Ptt(object):
     def is_connect(self):
         self._content = self._telnet.read_very_eager().decode('big5', 'ignore')
         if u"系統過載" in self._content:
-            print('server overload , try later')
+            logging.info('server overload , try later')
             sys.exit(0)
         return True
 
     def login(self):
         if self.input_user_password:
-            print("login success")
+            logging.info("login success")
             return True
-        print("ptt down, login fail")
+        logging.info("ptt down, login fail")
         return False
 
     def logout(self):
-        print("logging out...")
+        logging.info("logging out...")
 
         self._telnet.write(b"qqqqqqqqqg\r\ny\r\n")
         time.sleep(1)
         self._telnet.close()
-        print("logout success")
+        logging.info("logout success")
 
     def post(self, board, title, content):
-        print("發文中...")
+        logging.info("發文中...")
         # s 進入要發文的看板
         self._telnet.write(b's')
         self._telnet.write(board.encode('big5') + b'\r\n')
@@ -94,9 +103,9 @@ class Ptt(object):
         self._telnet.write(b's\r\n')
         # 不加簽名檔
         self._telnet.write(b'0\r\n')
-        print("----------------------------------------------")
-        print("------------------ 發文成功 ------------------")
-        print("----------------------------------------------")
+        logging.info("----------------------------------------------")
+        logging.info("------------------ 發文成功 ------------------")
+        logging.info("----------------------------------------------")
 
 
 def main():
@@ -107,11 +116,11 @@ def main():
         ptt = Ptt(host, user, password[i])
         time.sleep(1)
         if ptt.is_connect():
-            print("using acct {}".format(user))
+            logging.info("using acct {}".format(user))
             if ptt.login():
                 pass
         ptt.logout()
-    print("done")
+    logging.info("done")
 
 
 if __name__ == "__main__":
